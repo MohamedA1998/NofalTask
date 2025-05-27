9a126c2fc53affb0ff14eefe9c4fa2b4a0b26d3c
Mohamed Alsayed <malsayed493@gmail.com>
2025-05-27 03:41:20 +0300

    some update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\CountriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('countries', CountriesController::class)->only('index', 'show');
